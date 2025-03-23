6c24949a948d511620313e7588af0a963833caeb
edm-r <edmondyopa22@gmail.com>
2025-03-23 18:17:35 +0100

    update models

--- project_/models.py
+++ project_/models.py
@@ -18,7 +18,7 @@ class Project(models.Model):
     reference_number = models.CharField(max_length=20, unique=True, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    objectives = models.TextField()
+    objectives = models.TextField(blank=True, null=True)
     deadline = models.DateField()
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='in_progress')
     owner = models.ForeignKey(
